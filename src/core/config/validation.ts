cb705953e245ff351aa5941f1bd2f1505e225226
Nawatt Y. <golf.enfc40@gmail.com>
2025-06-04 22:48:41 +0700

    แก้คำผิด

--- src/core/config/validation.ts
+++ src/core/config/validation.ts
@@ -4,7 +4,7 @@ export default Joi.object({
   NODE_ENV: Joi.string()
     .valid('local', 'develop', 'staging', 'uat', 'production', 'test')
     .default('local'),
-  APP_HOST: Joi.string().default('localhsot'),
+  APP_HOST: Joi.string().default('localhost'),
   APP_PORT: Joi.number().default(3000),
   APP_NAME: Joi.string().required(),
 });
