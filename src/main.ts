9ae1de7854d3a236c83ed309c0e45cca1aa3ee81
Nawatt Y. <golf.enfc40@gmail.com>
2025-06-05 20:09:47 +0700

    ปรับ response จาก validate เพื่อให้ฝั่งเรียกใช้นำไป implement ได้ง่ายขึ้น

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory, Reflector } from '@nestjs/core';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '#app.module';
 import { HttpExceptionFilter } from '#shared/filters/http-exception.filter';
 import { GlobalSerializerInterceptor } from '#shared/interceptors/global-serializer.interceptor';
 import { setupSwagger } from '#config/swagger.config';
 import { CoreConfigService } from '#core/config/config.service';
+import { ValidationError } from 'class-validator';
+import { ERROR_CODE } from '#shared/constants/error-code.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,9 +32,24 @@ async function bootstrap() {
   // ✅ เปิด ValidationPipe แบบ global
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // ตัด field ที่ไม่อยู่ใน DTO
-      forbidNonWhitelisted: true, // ถ้ามี field เกินมา → throw error
-      transform: true, // แปลง primitive (เช่น string → number) ตาม type ของ DTO
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        const errors = validationErrors.reduce((accumulator, currentValue) => {
+          accumulator[currentValue.property] = Object.values(
+            currentValue.constraints ?? {},
+          );
+          return accumulator;
+        }, {});
+
+        // สร้าง Exception object ในรูปแบบที่ HttpExceptionFilter รู้จัก
+        return new BadRequestException({
+          errorCode: ERROR_CODE.VALIDATE_ERROR,
+          errorMessage: 'VALIDATE_ERROR',
+          errors: errors,
+        });
+      },
     }),
   );
 
--- src/modules/auth/swagger/login.response.ts
+++ src/modules/auth/swagger/login.response.ts
@@ -2,7 +2,7 @@ const authSuccessJson =
   '{"accessToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkODBkNTlkNi1lYWI3LTRkNWYtYWFlNS05ZWNlOWQ1MzU2MzMiLCJpYXQiOjE3NDg5NDM4MzIsImV4cCI6MTc0ODk0NzQzMn0.9HPPDoFrWDbyvMblt-_J9LKsQH3V0PLcf0tYvPXqot8","refreshToken":"83929ded-b2fd-42f3-871a-59847cbdff35","userId":"d80d59d6-eab7-4d5f-aae5-9ece9d535633"}';
 
 const validateError =
-  '{"status":{"code":400,"message":"Bad Request"},"error":{"code":100422,"message":"VALIDATE_ERROR","errors":["username should not be empty","password should not be empty","password must be a string"]},"path":"/api/auth/login","timestamp":"2025-06-03T09:42:06.643Z"}';
+  '{"status":{"code":400,"message":"Bad Request"},"error":{"code":100422,"message":"VALIDATE_ERROR","errors":{"username":["username should not be empty"],"password":["password should not be empty","password must be a string"]}},"path":"/api/auth/login","timestamp":"2025-06-05T13:04:46.410Z"}';
 
 const credentialsMismatchError =
   '{"status":{"code":401,"message":"Unauthorized"},"error":{"code":101401,"message":"Username or password is incorrect","errors":[]},"path":"/api/auth/login","timestamp":"2025-06-03T09:39:40.604Z"}';
